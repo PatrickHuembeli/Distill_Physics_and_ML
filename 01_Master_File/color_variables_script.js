650a8a63f1761a91a4604bd88b59055174fa4689
Patrick Huembeli <patrick.huembeli@icfo.es>
2019-12-13 18:37:44 +0100

    Figure 2 cleaned

--- 01_Master_File/color_variables_script.js
+++ 01_Master_File/color_variables_script.js
@@ -12,3 +12,15 @@ var 	c_histo_bars = "blue" 			//Color of the histogram bars
 	c_text_slider = "grey" 			// Slider Text color
 	c_text_histogram = "grey" 		// Histogram text color
 
+// COLORS FIGURE 2 (ARCHITECTURE)
+
+var 	c_vis_node1 = "white" 			// Color for visible nodes
+	c_vis_node2 = "black" 			// Hidden nodes colors are defined in Figure 1
+	c_vis_node1_stroke = "black"
+	c_vis_node2_stroke = "black"
+	c_architecture_connection = "grey"  	// Connections color in architectures
+
+	// OTHER STYLE PARAMETERS FOR FIGURE 2
+	var 	connection_stroke_width = 2 	// width of the connection lines
+		nodes_stroke_width = 1 		// Stroke width of the nodes
+		nodes_radius = 12
