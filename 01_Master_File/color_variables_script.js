3ef2db1fc4fb41d75c697dd9a3d0ab7862fe5329
Patrick Huembeli <patrick.huembeli@icfo.es>
2019-12-13 16:59:10 +0100

    Cleaned 1st figure

---
+++ 01_Master_File/color_variables_script.js
@@ -0,0 +1,14 @@
+
+// COLORS FIGURE 1 (Temperature, coupling and probability)
+var 	c_histo_bars = "blue"
+	c_hid_node1 = "rgb(0,0,255,0.5)"
+	c_hid_node2 = "rgb(255,0,0,0.5)"
+	c_hid_node1_stroke = "blue"
+	c_hid_node2_stroke = "red"
+	c1_2D_slider = "white"
+	c2_2D_slider = "grey"
+	c_handle_2D_slider = "orange"
+	c_stroke_2D_slider = "white"
+	c_text_slider = "grey"
+	c_text_histogram = "grey"
+
